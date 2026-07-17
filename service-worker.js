190427e8fe335f8a4266fc5c1b4f958e5298736f
SivaSinaga <sivasinaga1105@gmail.com>
2026-07-17 13:34:07 +0700

    Update service worker to version v2 and assets

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,33 @@
-const CACHE_NAME = "gemanusa-lite-v1";
+const CACHE_NAME = "gemanusa-v2";
+
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./assets/icon.svg",
-  "./assets/wave.svg"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
